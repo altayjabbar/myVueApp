56e1da103e8a129190f210e68ccbce1d38e31014
altayjabbar <jabbarofaltay@gmail.com>
2025-02-28 00:18:45 +0400

    task3

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,23 +1,25 @@
 Vue.createApp({
-    data() {
-      return {
-        user: "",
-      };
+  data() {
+    return {
+      user: "",
+
+
+    };
+  },
+
+  methods: {
+    showAlert() {
+      console.log("hi")
+      alert("show Alert");
     },
-    methods: {
-      showAlert() {
-        console.log("hi")
-        alert("show Alert");
-      },
-      register(event) {
-        this.user = event.target.value; 
-      },
-      check() {
-        console.log(this.user); 
-      },
-      reset(){
-        this.user = "";
-      }
+    register(event) {
+      this.user = event.target.value;
+    },
+    check() {
+      console.log(this.user);
+    },
+    reset() {
+      this.user = "";
     }
-  }).mount("#assignment");
-  
+  }
+}).mount("#assignment");
