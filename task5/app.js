0e96e9f35d3e2cedf51bb82df9b448e7f82ce0a0
altayjabbar <jabbarofaltay@gmail.com>
2025-03-07 08:33:51 +0400

    assignment(task5)

--- task5/app.js
+++ task5/app.js
@@ -1,8 +1,25 @@
 Vue.createApp({
     data() {
         return {
-            "user1_css": user1,
-            "user2": user2,
+            inputClass: "",
+            paragraphisvisible: true,
+            inputBackgroundColor: ""
+        };
+    },
+    computed: {
+        paraClasses() {
+            return {
+                user1: this.inputClass === 'user1',
+                user2: this.inputClass === 'user2',
+                visible: this.paragraphisvisible,
+                hidden:!this.paragraphisvisible
+            }
+        }
+
+    },
+    methods: {
+        toggleparagraphvisibility() {
+            this.paragraphisvisible = !this.paragraphisvisible
         }
     }
-})
+}).mount("#assignment")
