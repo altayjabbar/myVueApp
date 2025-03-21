8eb58140c281f6b6593d6003f5a374c2cecd76eb
altayjabbar <jabbarofaltay@gmail.com>
2025-03-22 01:22:06 +0400

    validating props

--- my-vue-projet/src/components/FriendContact.vue
+++ my-vue-projet/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite==='1' ? '(Favorite)':'' }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : "" }}</h2>
     <button @click="toggleFavorite">toggle Favorite</button>
     <button @click="toogleDetails">
       {{ detaisVisible ? "Hide" : "Show" }} Details
@@ -9,44 +9,53 @@
       <li><strong>Phone:</strong>{{ phoneNumber }}</li>
 
       <li><strong>Email:</strong>{{ emailAddress }}</li>
-    </ul> 
+    </ul>
   </li>
 </template>
 
 <script>
-
-export default{
-  props:[
-  'name',
-  'phoneNumber',
-  'emailAddress',
-  'isFavorite'
-],
-    data(){
-        return{
-            detaisVisible: false,
-            friend:{
-                id: "manuel",
-                name: "Manuel Lorenz",
-                phone: "01245 546 54",
-                email: "manuel@gmail.com",
-            },
-            friendIsFavorite:this.isFavorite
-
-            
-        };
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
     },
-    methods:{
-        toogleDetails(){
-            this.detaisVisible=!this.detaisVisible   
-        },
-        toggleFavorite(){
-          if(this.friendIsFavorite==='1'){
-            this.friendIsFavorite='0';
-          }else{
-            this.friendIsFavorite='1';
-          }
-        }
-    }
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      detaisVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01245 546 54",
+        email: "manuel@gmail.com",
+      },
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toogleDetails() {
+      this.detaisVisible = !this.detaisVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
+      }
+    },
+  },
 };
 </script>
