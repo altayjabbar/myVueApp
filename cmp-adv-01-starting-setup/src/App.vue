787e84a4d2fc4dd46db6546bc541a4a268cb0031
altayjabbar <jabbarofaltay@gmail.com>
2025-04-04 00:55:15 +0400

    added dynamic components and scoped slot

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
+    <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
+
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals", // Dəyişənin adı burada düzgün yazılıb
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp; // Dəyişənin adı burada da düzgün təyin olunub
+    },
+  },
+};
+</script>
+
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ cmp-adv-01-starting-setup/src/components/BadgeList.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <base-card>
+    <template v-slot:header>
+    </template>
+      <ul>
+        <li>
+          <base-badge type="admin" caption="ADMIN"></base-badge>
+        </li>
+        <li>
+          <base-badge type="author" caption="AUTHOR"></base-badge>
+        </li>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<style scoped>
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+}
+
+li {
+  margin-right: 1rem;
+}
+</style>
