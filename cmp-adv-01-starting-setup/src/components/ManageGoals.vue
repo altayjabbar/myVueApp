22684c46f6394722b63f8bae040e1aa04ff8a819
altayjabbar <jabbarofaltay@gmail.com>
2025-04-05 16:21:18 +0400

    dynamic component

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,3 +1,40 @@
 <template>
-  <h2>Manage Goals</h2>
+  <div>
+    <h2>Manage Goals</h2>
+    <button @click="setGoal" ref="goal">Set Goal</button>
+    <error-alert v-if="inputisValid">
+      <h2>Input is inValid</h2>
+      <p>Please enter at least a few chacacters...</p>
+      <button @click="ErrorHandle">Okay</button>
+    </error-alert>
+    <input type="text" />
+  </div>
 </template>
+
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default{
+
+  components:{
+    ErrorAlert,
+  },
+  data(){
+    return{
+      inputisValid:false,
+    }
+  },
+  methods:{
+    setGoal(){
+     const enteredValue=this.$refs.goal.value;
+     if (enteredValue===''){
+      this.inputisValid=true;
+     }
+    },
+    ErrorHandle(){
+      this.inputisValid=false;
+    }
+  }
+}
+</script>
