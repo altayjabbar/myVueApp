30aeb91da9b1ec65e2f4ccb4a8595289acff65a2
altayjabbar <jabbarofaltay@gmail.com>
2025-04-10 01:18:03 +0400

    first components ,props

---
+++ project-01/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The official Google documentation',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
