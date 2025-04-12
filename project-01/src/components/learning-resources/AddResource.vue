a2d95ef01ab5d001cff6a2b6ab6bfdc70df6c3b7
altayjabbar <jabbarofaltay@gmail.com>
2025-04-12 09:58:49 +0400

    dynamic componenets

---
+++ project-01/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Add Resouce</h2>
+</template>
---
+++ project-01/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,36 @@
+<template>
+    <base-card>
+      <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+      <base-button @click="setSelectedTab('add-resource')">Add Resources</base-button>
+      
+      <component :is="selectedTabComponent" />
+    </base-card>
+  </template>
+  
+  <script>
+  import StoredResources from './StoredResources.vue';
+  import AddResource from './AddResource.vue';
+  
+  export default {
+    components: {
+      StoredResources,
+      AddResource,
+    },
+    data() {
+      return {
+        selectedTab: 'stored-resources',
+      };
+    },
+    methods: {
+      setSelectedTab(tab) {
+        this.selectedTab = tab;
+      },
+    },
+    computed: {
+      selectedTabComponent() {
+        return this.selectedTab === 'stored-resources' ? 'StoredResources' : 'AddResource';
+      },
+    },
+  };
+  </script>
+  
