77e87b6c8761b452d84cd22324a68f4de289de22
altayjabbar <jabbarofaltay@gmail.com>
2025-05-15 13:51:04 +0400

    passing data with payload

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <div>
+    <base-container title="Vuex"></base-container>
+    <the-components></the-components>
+    <button @click="addOne">Add 10</button>
+  </div>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
-
+import TheComponents from './components/TheComponents.vue';
 export default {
   components: {
     BaseContainer,
+    TheComponents,
+  },
+
+  methods: {
+    addOne() {
+      this.$store.commit('increase', { value: 10 });
+    },
   },
 };
 </script>
